ba56ab932d700c7fa12a604a6f9709b405be00e8
SadmanJawad <jawadnabil977@gmail.com>
2023-05-10 02:01:17 +0600

    send coffee data to the server

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -14,6 +14,21 @@ const AddCoffee = () => {
 
         const newCoffee = {name, quantity, taste, supplier, category, details, photo}
         console.log(newCoffee)
+        
+        // send the data to the server
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+
 
     }
 
